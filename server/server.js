f92b959d6f4378ce097066f9c6ceb728c8f3a0d2
Bela Hamid <belahamid@gmail.com>
2021-05-16 22:35:45 +0100

    add data to project directory

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res){
-    res.send('Hello world')
+    res.json({message: "Hello world!"});
 });
 
 app.listen(3000, function(){
