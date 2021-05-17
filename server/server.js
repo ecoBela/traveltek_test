71afa68657def382e226b53039a37b4b7624c0e7
Bela Hamid <belahamid@gmail.com>
2021-05-17 13:11:21 +0100

    api endpoint '/' can output xml flight data as json

--- server/server.js
+++ server/server.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const app = express();
 
-const fs = require('fs');
+// var parseString = require('xml2js').parseString;
 
+var fs = require('fs'),
+    xml2js = require('xml2js');
 
-fs.readFile('./db/flightdata_A.xml', (err, data) => {
-    if(err){
-        console.log('error')
-    }
-    console.log(data.toJSON());
+var parser = new xml2js.Parser();
+
+const flightResults = [];
+
+fs.readFile('/Users/belahamid/post_codeclan/traveltek/traveltek_challenge/server/db/test_data.xml', function(err, data) {
+    parser.parseString(data, function (err, result) {
+        console.dir(result.flights.flight);
+        console.log('Done');
+        flightResults.push(result);
+        
+    });
 });
 
 
-app.get('/', function (req, res){
-    // res.json({message: "Hello world!"});
+app.get('/', function(req, res){
+    // res.send({ message: "Hello World!" });
+    res.send({flightResults})
+
+});
+
+app.get('/flights', function(req, res){
+    res.send({message: "Flight stats"});
 });
 
 app.listen(3000, function(){
