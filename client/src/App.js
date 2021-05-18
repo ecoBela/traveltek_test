e280e789eaeb1f03773af702e0c213dee8db109c
Bela Hamid <belahamid@gmail.com>
2021-05-18 20:20:41 +0100

    function to find day with most departures from Manchester partly works

--- client/src/App.js
+++ client/src/App.js
@@ -2,37 +2,32 @@ import './App.css';
 import React, {useEffect, useState} from 'react';
 
 function App() {
-//const [flightWithMostStops, setFlightWithMostStops] = useState();
-//const [dayWtihMostDeparturesManchester, setDayWithMostDeparturesManchester] = useState();
-//const [noOfDiffFlightsOnMon] = useState();
-//const [noOfDiffFlightsOnTues] = useState();
-//const [noOfDiffFlightsOnWed] = useState();
-//const [noOfDiffFlightsOnThurs] = useState();
-//const [noOfDiffFlightsOnFri] = useState();
-//const [noOfDiffFlightsOnSat] = useState();
-//const [noOfDiffFlightsOnSun] = useState();
-//const [propOfBusinessFlights, setPropOfBusinessFlights] - useState();
-//const [interestingStat, setInterestingStat] = useState();
 
-//FETCH your data - once it all works - swap TEST data file with the FULL DATA set****
-const AllFlightData = function(){
+const [allFlights, setAllFlights] = useState([]);
+const [mondayFlightsManchester, setMondayFlightsManchester] = useState(0); 
+const [tuesdayFlightsManchester, setTuesdayFlightsManchester] = useState(0); 
+const [wednesdayFlightsManchester, setWednesdayFlightsManchester] = useState(0); 
+const [thursdayFlightsManchester, setThursdayFlightsManchester] = useState(0); 
+const [fridayFlightsManchester, setFridayFlightsManchester] = useState(0); 
+const [saturdayFlightsManchester, setSaturdayFlightsManchester] = useState(0); 
+const [sundayFlightsManchester, setSundayFlightsManchester] = useState(0); 
+
+
+
+const getAllFlightData = function(){
   console.log("getting data...")
   fetch('http://localhost:3000/flights')
   .then(response => response.json())
-  .then(data =>console.log(data))
+  .then(data =>setAllFlights(data))
 }
 
-// getAllFlightData();
-//USE effect to fetch the data on load
+
 useEffect(() => {
-  AllFlightData()
+  getAllFlightData()
 }, [])
 
 
-//1. function to find flight with most stops
 //2. function to find day with most departures from Manchester
-
-const findDayWithMostDepManchester = function(){
 const depMon = []
 const depTue =[]
 const depWed =[]
@@ -41,42 +36,66 @@ const depFri = []
 const depSat =[]
 const depSun = []
 
-AllFlightData.forEach(flight => {
-  if(flight.$.depair === 'MAN'){
+const findDayWithMostDepManchester = function(){
+
+allFlights.allFlights[0].forEach(flight => {
+
+  if(flight['$'].depair === 'MAN'){
     const depDate = new Date(flight.$.outdepartdate);
-//then look for the day and push object into correct day array
+    const depDay = depDate.getDay();
+    if(depDay === 0){
+      depSun.push(flight)
+    }
+    if(depDay ===1){
+      depMon.push(flight)
+    }
+    if(depDay === 2){
+      depTue.push(flight)
+    }
+    if(depDay ===3){
+      depWed.push(flight)
+    }if(depDay === 4){
+      depThur.push(flight)
+    }
+    if(depDay ===5){
+      depFri.push(flight)
+    }
+    if(depDay ===6){
+      depSat.push(flight)
+    }
   }
-  if(flight.segments.segment.$.depcode === 'MAN'
-  && flight.segments.segment.$.journey === 'out'){
-    //then look for the day and push this object into day array
-  }
-  
 });
-
-
-//for each flight object in array, if $.depair === MAN  (and check it's an 'out' not 'in')
-
-//then store departure date as a new Date object
-//find the date that it equates to and store that in a variable
-//if the day is monday then put it in monday array, if tues etc
-//count length of each array
-//console.log the day with the most departures
-
-
+console.log(depMon.length, depTue.length);
+setMondayFlightsManchester(depMon.length);
+setTuesdayFlightsManchester(depTue.length);
+setWednesdayFlightsManchester(depWed.length);
+setThursdayFlightsManchester(depThur.length);
+setFridayFlightsManchester(depFri.length);
+setSaturdayFlightsManchester(depSat.length);
+setSundayFlightsManchester(depSun.length);
 }
-
-
-
-//3. number of different flights on each day of the week: mon, tues etc
-//4. function to find proportion of flights that are business class
-//5. function to find one other interesting stat
 
 
   return (
     <div className="App">
       <h1>Dashboard</h1>
       <p>Which flight has the most stops? [INSERT {}] </p>
-      <p>Day with most departures from Manchester (MAN): </p>
+
+      
+
+      <p>Click button and review below to find day with most departures from Manchester (MAN):</p>
+      <button onClick={findDayWithMostDepManchester}>Click me </button>
+        <ul>
+          <li>Monday: {mondayFlightsManchester}</li>
+          <li>Tuesday: {tuesdayFlightsManchester} </li>
+          <li>Wednesday: {wednesdayFlightsManchester}</li>
+          <li>Thursday: {thursdayFlightsManchester}</li>
+          <li>Friday: {fridayFlightsManchester}</li>
+          <li>Saturday: {saturdayFlightsManchester} </li>
+          <li>Sunday: {sundayFlightsManchester} </li>
+        </ul>
+        
+
       <p>How many different flight numbers on Monday: {}, Tuesday: {}, Wednesday: {}, Thursday: {}, Friday: {}, Saturday: {}, Sunday: {}</p>
       <p>Which proportion of the flights are business class?</p>
       <p>One other interesting statistic: {}</p>
